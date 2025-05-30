742b105bedeced0d88cc9f943d69283d8ebeba43
Ryota Fujisawa <ninjafire72@live.com>
2025-05-30 12:27:31 +1000

    Update PluginController.php

--- controllers/PluginController.php
+++ controllers/PluginController.php
@@ -8,9 +8,6 @@ class PluginController
     static function init()
     {
         Settings::init_settings();
-        
-        $filter = 'plugin_action_links_' . {PREFIX}_PLUGIN_PATH;
-        add_filter($filter, [self::class, 'actionLinksFilter']);
     }
 
     static function actionLinksFilter($actions) {
